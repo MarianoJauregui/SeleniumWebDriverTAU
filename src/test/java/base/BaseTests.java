a4481d60aaaf6a3baf13dd629bae1e29b99220c3
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-05-12 08:41:51 -0300

    Finding elements on WebDriver

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
@@ -12,11 +16,21 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        /*
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+
         driver.manage().window().maximize();
         driver.manage().window().setSize(new Dimension(375,812));
+         */
+
 
         System.out.println(driver.getTitle());
         //driver.quit();
+        //driver.close(); closes the window, not the session.
     }
     public static void main(String[] args) {
         BaseTests test = new BaseTests();
