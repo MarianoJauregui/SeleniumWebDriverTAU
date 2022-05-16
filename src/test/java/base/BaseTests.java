b70639d938699b82be773e8b28f1090e109e07fd
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-05-16 17:31:44 -0300

    Element interactions - 2

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,41 +1,28 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import pages.HomePage;
 
-import java.util.List;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
 
+    @BeforeClass
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
+
         driver.get("https://the-internet.herokuapp.com/");
-        System.out.println(driver.getTitle());
-
         homePage = new HomePage(driver);
-
-        driver.quit();
-
-        /*
-        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        inputsLink.click();
-
-        driver.manage().window().maximize();
-        driver.manage().window().setSize(new Dimension(375,812));
-         */
-
-        //driver.close(); closes the window, not the session.
     }
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setup();
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
 }
 
