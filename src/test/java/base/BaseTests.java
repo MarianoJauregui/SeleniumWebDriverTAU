4f847e8c704cda27cdf9f255bb4c158194fc8482
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-05-16 08:49:30 -0300

    Element interactions

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,19 +5,23 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
+        homePage = new HomePage(driver);
+
+        driver.quit();
 
         /*
         WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
@@ -27,9 +31,6 @@ public class BaseTests {
         driver.manage().window().setSize(new Dimension(375,812));
          */
 
-
-        System.out.println(driver.getTitle());
-        //driver.quit();
         //driver.close(); closes the window, not the session.
     }
     public static void main(String[] args) {
