be46a7bf57e31403ff6f7516d3b64475b7286bc6
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-05-17 10:03:57 -0300

    Interacting with dropdown elements - Lambda, collections and streams.

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Page Object Model design.
+ */
+
+public class HomePage {
+    private WebDriver driver;
+
+    //private By formAuthenticationLink = By.linkText("Form Authentication");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public LoginPage clickFormAuthentication() {
+        clickLink("Form Authentication");
+        //driver.findElement(formAuthenticationLink).click();
+        return new LoginPage(driver);
+    }
+
+    public DropdownPage clickDropdown(){
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
+
+    //Generic Method so it does not return anything.
+    private void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+    }
+}
